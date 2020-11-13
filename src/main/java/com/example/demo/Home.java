e63dfaec2aca347b80cdde0f971525ab7a28b102
tmd3282 <tmd3282@nhn.com>
2020-11-13 14:46:33 +0900

    [DongSeungLee] feature/merge-test : test1

--- src/main/java/com/example/demo/Home.java
+++ src/main/java/com/example/demo/Home.java
@@ -161,7 +161,7 @@ public class Home {
 //        testService.hoho();
 //        log.info("after test service");
 //        testService.func(10);
-        //sendFavorite();
+//        sendFavorite();
     }
 
     public Home(TestService testService
