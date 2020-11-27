52b3d54a14a67d64e239db74f8354e16a2603fde
tmd3282 <tmd3282@nhn.com>
2020-11-27 15:04:23 +0900

    [DongSeungLee] develop : add comments to TemporalAdjuster

--- src/test/java/com/example/demo/LocalDateTimeTest.java
+++ src/test/java/com/example/demo/LocalDateTimeTest.java
@@ -28,11 +28,12 @@ public class LocalDateTimeTest {
     @Test
     public void test_TemporalAdjusters() {
         LocalDate date1 = LocalDate.of(2020, 11, 25);
-        //  이 주 혹은 그 다음주의 날을 return.
+        //  이 주 혹은 그 다음주의 같은 요일의 날을 return.
         LocalDate date2 = date1.with(nextOrSame(DayOfWeek.SUNDAY));
         LocalDateTime here = LocalDateTime.of(date2.getYear(), date2.getMonthValue(), date2.getDayOfMonth(), 0, 0, 0);
         System.out.println(here);
         System.out.println(date2);
+        // LocalDate에서의 해당 달의 마지막 날, LocalDate를 return한다.
         LocalDate date3 = date2.with(lastDayOfMonth());
         System.out.println(String.format("last day of %s is %s", date3.getMonth(), date3.getDayOfWeek()));
 //        System.out.println(date3);
@@ -40,7 +41,9 @@ public class LocalDateTimeTest {
 //        System.out.println(here.with(DSTemporalAdjuster.nextWorkingDay()));
         ZoneId seoul = ZoneId.of("Asia/Seoul");
         ZoneId la = ZoneId.of("America/Los_Angeles");
+        // 현재 구한 시간의 시간대를 한국의 Seoul로 결정
         System.out.println(LocalDateTime.now().atZone(seoul));
+        // 현재 구한 시간의 시간대를 LA의 시간으로 결정, 이렇게 해야 DST(Daylight Saving Time) = summer time과 같은 복잡한 시간도 처리해줄 수 있다.
         System.out.println(LocalDateTime.now().atZone(la));
         System.out.println(Instant.ofEpochMilli(System.currentTimeMillis()).getEpochSecond());
 
